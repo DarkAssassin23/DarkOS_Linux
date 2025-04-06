b605d1649549c1c20edb566d04d3b5a8e5dafe59
DarkAssassin23 <15916504+DarkAssassin23@users.noreply.github.com>
2025-04-06 12:21:10 -0500

    Used config file to get ISO name, added system prune to clean up phase

--- dockerBuildExtract.sh
+++ dockerBuildExtract.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
+. ./config.sh
+
 # Create the environmnet and build the ISO
 docker build -t linux-build .
 
 # Extract the ISO
 container=$(docker create linux-build)
-docker container run $container
-docker cp "$container:/server/tmp/darkos_linux.iso" .
+docker cp "$container:/server/tmp/$ISO" .
 
 # Clean up
 docker rm "$container"
 docker image rm linux-build
+docker system prune -f
 
